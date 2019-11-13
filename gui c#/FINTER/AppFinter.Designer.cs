e2763a4f9b37a0770ab494ad4f73ce3fd25afaf9
FaqMmb <facundo.mbm96@gmail.com>
2019-11-13 20:22:40 -0300

    finter c#

---
+++ gui c#/FINTER/AppFinter.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace FINTER
+{
+    partial class AppFinter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cmbMetodos = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(370, 21);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(144, 58);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(321, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingrese valores de x (seguidos de una coma)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(293, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ingrese valores de y (seguidos de una coma)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(321, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // cmbMetodos
+            // 
+            this.cmbMetodos.FormattingEnabled = true;
+            this.cmbMetodos.Location = new System.Drawing.Point(321, 152);
+            this.cmbMetodos.Name = "cmbMetodos";
+            this.cmbMetodos.Size = new System.Drawing.Size(175, 24);
+            this.cmbMetodos.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(225, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Seleccion metodo de interpolacion";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(14, 30);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(46, 17);
+            this.lblResultado.TabIndex = 7;
+            this.lblResultado.Text = "label4";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.lblResultado);
+            this.groupBox1.Location = new System.Drawing.Point(15, 306);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(521, 133);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnCalcular);
+            this.groupBox2.Location = new System.Drawing.Point(15, 199);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(520, 121);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(370, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 62);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "MOSTRAR PASOS DE CALCULO";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // AppFinter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 451);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbMetodos);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "AppFinter";
+            this.Text = "FINTER";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox cmbMetodos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ gui c#/FINTER/AppFinter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FINTER.Calculos;
+
+namespace FINTER
+{
+    public partial class AppFinter : Form
+    {
+        public AppFinter()
+        {
+            InitializeComponent();
+            lblResultado.Hide();
+            cmbMetodos.Items.Add("Lagrange");
+            cmbMetodos.Items.Add("Newton-Gregory Progresivo");
+            cmbMetodos.Items.Add("Newton-Gregory Regresivo");
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+            if (cmbMetodos.SelectedItem.ToString() == "Newton-Gregory Progresivo") 
+            {
+                NewtonGregoryProg calculadora = new NewtonGregoryProg();
+
+
+            
+            }
+
+        }
+    }
+}
